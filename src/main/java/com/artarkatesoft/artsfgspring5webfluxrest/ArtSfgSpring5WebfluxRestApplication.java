d2b5017937f90b82b08d9522ed193de3db82366f
admin <d.art.shishkin@gmail.com>
2020-08-20 22:42:28 +0300

    New Spring Boot Project (407). closes #1

---
+++ src/main/java/com/artarkatesoft/artsfgspring5webfluxrest/ArtSfgSpring5WebfluxRestApplication.java
@@ -0,0 +1,13 @@
+package com.artarkatesoft.artsfgspring5webfluxrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtSfgSpring5WebfluxRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtSfgSpring5WebfluxRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artarkatesoft/artsfgspring5webfluxrest/ArtSfgSpring5WebfluxRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artarkatesoft.artsfgspring5webfluxrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtSfgSpring5WebfluxRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
